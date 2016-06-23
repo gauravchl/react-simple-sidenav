7926431b49c1fb827be0d0e00a3e408b4eb6c1e6
gauravchl <gauravchl@yahoo.com>
2016-06-23 15:00:36 +0530

    example page updated with latet sideNav

--- example/index.js
+++ example/index.js
@@ -1,4 +1,85 @@
+
+
+var Layout = React.createClass({
+  displayName: 'Layout',
+  getInitialState: function getInitialState() {
+    return {
+      showNav: false
+    };
+  },
+  getSideNavContent: function getSideNavContent() {
+    var styles = {
+      title: {
+        background: '#E91E63',
+        color: '#fff',
+        fontWeight: 400,
+        margin: 0,
+        lineHeight: '82px',
+        padding: 22
+      },
+      li: {
+        padding: 22,
+        cursor: 'pointer'
+      }
+    };
+
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'h1',
+        { style: styles.title },
+        'Simple SideNav'
+      ),
+      React.createElement(
+        'ul',
+        null,
+        React.createElement(
+          'li',
+          { style: styles.li },
+          'Item 1'
+        ),
+        React.createElement(
+          'li',
+          { style: styles.li },
+          'Item 2'
+        )
+      )
+    );
+  },
+  render: function render() {
+    var _this = this;
+
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'div',
+        { style: { width: '100%', background: "#0AC" } },
+        React.createElement(
+          'div',
+          { style: { padding: 14 }, onClick: function onClick() {
+              return _this.setState({ showNav: !_this.state.showNav });
+            } },
+          React.createElement(
+            'svg',
+            { xmlns: 'http://www.w3.org/2000/svg', cursor: 'pointer', fill: '#fff', height: '24', viewBox: '0 0 24 24', width: '24' },
+            React.createElement('path', { d: 'M0 0h24v24H0z', fill: 'none' }),
+            React.createElement('path', { d: 'M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' })
+          )
+        )
+      ),
+      React.createElement(
+        SideNav,
+        { showNav: this.state.showNav, onHideNav: function onHideNav() {
+            return _this.setState({ showNav: false });
+          } },
+        this.getSideNavContent()
+      )
+    );
+  }
+});
 ReactDOM.render(
-  React.createElement("p", null, "react-simple-nav"),
+  React.createElement(Layout, null, null),
   document.getElementById('example')
 );
---
+++ example/sidenav.js
@@ -0,0 +1,90 @@
+SideNav = React.createClass({
+  displayName: 'SideNav',
+
+  propTypes: {
+    style: React.PropTypes.object,
+    navStyle: React.PropTypes.object,
+    children: React.PropTypes.node,
+    showNav: React.PropTypes.bool,
+    onHideNav: React.PropTypes.func,
+    onShowNav: React.PropTypes.func
+
+  },
+
+  getInitialState: function getInitialState() {
+    return {
+      showNav: this.props.showNav || false
+    };
+  },
+  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+    if (nextProps.showNav != this.props.showNav && nextProps.showNav != this.state.showNav) nextProps.showNav ? this.showNav() : this.hideNav();
+  },
+  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextStates) {
+    if (nextProps.showNav != this.props.showNav && nextProps.showNav == this.state.showNav) return false;
+    return true;
+  },
+  showNav: function showNav() {
+    this.refs.nav.style['transition'] = 'transform 0.33s cubic-bezier(0,0,0.3,1)';
+    this.setState({ showNav: true }, this.props.onShowNav);
+  },
+  hideNav: function hideNav() {
+    this.refs.nav.style['transition'] = 'transform 0.13s cubic-bezier(0,0,0.3,1)';
+    this.setState({ showNav: false }, this.props.onHideNav);
+  },
+  getStyle: function getStyle() {
+    var styles = {
+      root: {
+        left: 0,
+        top: 0,
+        width: '100%',
+        height: '100%',
+        position: 'fixed',
+        overflow: 'hidden',
+        zIndex: 8,
+        pointerEvents: this.state.showNav ? 'auto' : 'none'
+      },
+      nav: {
+        position: 'relative',
+        width: '90%',
+        maxWidth: 400,
+        height: '100%',
+        background: '#FFF',
+        boxShadow: '2px 0 12px rgba(0,0,0,0.4)',
+        transform: this.state.showNav ? 'none' : 'translateX(-102%)',
+        display: 'flex',
+        willChange: 'transform',
+        flexDirection: 'column'
+      },
+      overlay: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0,
+        opacity: this.state.showNav ? 1 : 0,
+        background: 'rgba(0,0,0,0.4)',
+        transition: 'opacity 0.3s cubic-bezier(0, 0, 0.3, 1)',
+        willChange: 'opacity'
+      }
+    };
+
+    Object.assign(styles.root, this.props.style);
+    Object.assign(styles.nav, this.props.navStyle);
+    return styles;
+  },
+  render: function render() {
+    var styles = this.getStyle();
+    return React.createElement(
+      'aside',
+      { style: styles.root },
+      React.createElement('div', { style: styles.overlay, onClick: this.hideNav }),
+      React.createElement(
+        'nav',
+        { style: styles.nav, onTransitionEnd: function onTransitionEnd(e) {
+            e.target.style['transition'] = 'none';
+          }, ref: 'nav' },
+        this.props.children || ''
+      )
+    );
+  }
+});
