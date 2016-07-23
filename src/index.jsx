8df39eeff69d579d265dd3a8db01678141d69bee
gauravchl <gauravchl@yahoo.com>
2016-07-23 14:59:12 +0530

    Add menu icon into SideNav component

--- src/index.jsx
+++ src/index.jsx
@@ -128,4 +128,19 @@ let SideNav = React.createClass({
   }
 });
 
+
+let MenuIcon = React.createClass({
+  render() {
+    return (
+      <svg {...this.props} xmlns="http://www.w3.org/2000/svg" cursor="pointer" fill="#fff" height="24" viewBox="0 0 24 24" width="24">
+        <path d="M0 0h24v24H0z" fill="none"/>
+        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+      </svg>
+    )
+  }
+});
+
+
+
+export {SideNav, MenuIcon};
 export default SideNav;
